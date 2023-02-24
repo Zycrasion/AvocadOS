f46ff82d5cbc3dbfbfe7c076b84e55f7a4e23f7c
Mars Kitten <57377187+MarsKittenPro@users.noreply.github.com>
2023-02-24 19:29:39 +1100

    adding Writer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![no_std]
 #![no_main]
-
+mod vga_buffer;
+use vga_buffer::{Color, ColorCode, Writer, Buffer};
 use core::panic::{PanicInfo};
+use core::fmt::Write;
 
 #[panic_handler]
 fn panic(_info : &PanicInfo) -> !
@@ -9,21 +11,19 @@ fn panic(_info : &PanicInfo) -> !
     loop {}
 }
 
-static HELLO : &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> !
 {
-    let vga_buffer = 0xb8000 as *mut u8;
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
 
-    for (i, &byte) in HELLO.iter().enumerate()
-    {
-        unsafe
-        {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    writer.write_string("a");
+
+    writeln!(writer, "test {}", 2).unwrap();
 
     loop {}
 }
