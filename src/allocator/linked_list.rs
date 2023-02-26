9f64b1547776515c47b178f58f3b32af0c885edf
Zycrasion <hendrix.may@gmail.com>
2023-02-26 19:47:40 +1100

    no more warnings

--- src/allocator/linked_list.rs
+++ src/allocator/linked_list.rs
@@ -46,7 +46,6 @@ impl LinkedListAllocator
     
     unsafe fn add_free_region(&mut self, addr: usize, size : usize)
     {
-        use super::align_up;
         use core::mem;
         assert_eq!(align_up(addr, mem::align_of::<ListNode>()), addr);
         assert!(size >= mem::size_of::<ListNode>());
